46c81862a39c9c1a7dc4ad7edb1f30c81ddff5b3
Renan Felipe <rennan_fellipe@yahoo.com.br>
2019-10-07 10:48:16 -0300

    Fix some backend problems

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
