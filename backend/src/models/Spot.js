ab54546c38c7b0167e433adef5913f28de8c353f
Renan Felipe <rennan_fellipe@yahoo.com.br>
2019-10-16 01:57:56 -0300

    Put api URL on .env file

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const SpotSchema = new mongoose.Schema(
@@ -20,7 +21,7 @@ const SpotSchema = new mongoose.Schema(
 );
 
 SpotSchema.virtual('thumbnail_url').get(function() {
-  return `http://localhost:3333/files/${this.thumbnail}`;
+  return `${process.env.URL || 'http://localhost'}:${process.env.PORT || 3333}/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
